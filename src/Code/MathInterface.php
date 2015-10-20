a03fafa0aca676c20f9e725d0f38f6ad99768edb
Claudio Zizza <claudio@budgegeria.de>
2015-10-20 22:51:30 +0200

    New Math class for test examples

--- src/Code/MathInterface.php
+++ src/Code/MathInterface.php
@@ -4,5 +4,11 @@ namespace MockMockMock\Code;
 
 interface MathInterface
 {
+    /**
+     * @param int|float $value1
+     * @param int|float $value2
+     *
+     * @return int|float
+     */
     public function sum($value1, $value2);
 }
